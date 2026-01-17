b0e1090fad2510158eb8b88e86972969e71d59c7
len-kadner <len.kadner.business.com>
2026-01-17 18:04:14 +0100

    Important Test

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ if ($_POST) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Social - Login</title>
+    <title>Social - LOGIN</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
